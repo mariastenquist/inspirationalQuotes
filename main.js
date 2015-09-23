bc9b1ca12e5e614f524e7708ad5df1067f6dc0b9
Student <student@RU-001.local>
2015-09-23 14:17:41 -0600

    next commit

--- main.js
+++ main.js
@@ -7,28 +7,33 @@ angular.module('quotesApp').controller('mainController', ['$scope', '$localStora
 
 		{
 			author: "Peter Thiel",
-			saying: "Encouraging young people to get real educations in the real world"
+			saying: "Encouraging young people to get real educations in the real world",
+			rating: 5
 		},
 
 		{
 			author: "Peter Thiel",
-			saying: "Encouraging young people to get real educations in the real world"
+			saying: "Encouraging young people to get real educations in the real world",
+			rating: 4
 		},
 
 		{
 			author: "Peter Thiel",
-			saying: "Encouraging young people to get real educations in the real world"
+			saying: "Encouraging young people to get real educations in the real world",
+			rating: 3
 		},
 
 
 		{
 			author: "Peter Thiel",
-			saying: "Encouraging young people to get real educations in the real world"
+			saying: "Encouraging young people to get real educations in the real world",
+			rating: 4
 		},
 
 		{
 			author: "Peter Thiel",
-			saying: "Encouraging young people to get real educations in the real world"
+			saying: "Encouraging young people to get real educations in the real world",
+			rating: 5
 		},
 
 		]
@@ -49,7 +54,22 @@ angular.module('quotesApp').controller('mainController', ['$scope', '$localStora
 
 		}
 
-//
+
+		$scope.rating =function(){
+
+			for (var i = 0; i < $scope.quotes.length; i++) {
+				$scope.quotes[i].stars = []
+					for (var j = 0; j < $scope.quotes[i].rating; j++) {
+						$scope.quotes[i].stars.push ('★')
+					};
+				
+
+			};
+		}
+	
+	$scope.rating()
+
+
 		$scope.delete = function($index){
 			$scope.quotes.splice($index,1)
 			console.log($index)
